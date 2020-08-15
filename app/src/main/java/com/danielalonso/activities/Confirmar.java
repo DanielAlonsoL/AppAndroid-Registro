d48db10e61cb78b41ff32372d8855a6436f56dee
Daniel Alonso <daan.alonso@gmail.com>
2020-08-14 22:59:46 -0500

    Se agregó botón para editar campos en el activity_confirmar.xml

--- app/src/main/java/com/danielalonso/activities/Confirmar.java
+++ app/src/main/java/com/danielalonso/activities/Confirmar.java
@@ -3,9 +3,11 @@ package com.danielalonso.activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textview.MaterialTextView;
 
 public class Confirmar extends AppCompatActivity {
@@ -16,6 +18,8 @@ public class Confirmar extends AppCompatActivity {
     private MaterialTextView set_telefono;
     private MaterialTextView set_email;
     private MaterialTextView set_descripcion;
+
+    private MaterialButton editar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,31 +47,16 @@ public class Confirmar extends AppCompatActivity {
         set_email.setText(email);
         set_descripcion.setText(descripcion);
 
+        editar = (MaterialButton) findViewById(R.id.btnEditar);
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-    }
-
-    private void setConfirma() {
-        extras = getIntent().getExtras();
-
-        String nombreCompleto = extras.getString(getString(R.string.p_nombre));
-        String fechaNacimiento = extras.getString(getString(R.string.p_fecha));
-        String telefono = extras.getString(getString(R.string.p_telefono));
-        String email = extras.getString(getString(R.string.p_email));
-        String descripcion = extras.getString(getString(R.string.p_descripcion));
-
-
-        set_nombre = (MaterialTextView) findViewById(R.id.muestraNombre);
-        set_fecha_nacimiento = (MaterialTextView) findViewById(R.id.muestraFechaNacimiento);
-        set_telefono = (MaterialTextView) findViewById(R.id.muestraTelefono);
-        set_email = (MaterialTextView) findViewById(R.id.muestraEmail);
-        set_descripcion = (MaterialTextView) findViewById(R.id.muestraDescripcion);
-
-        set_nombre.setText(nombreCompleto);
-        set_fecha_nacimiento.setText(fechaNacimiento);
-        set_telefono.setText(telefono);
-        set_email.setText(email);
-        set_descripcion.setText(descripcion);
     }
 
     @Override
